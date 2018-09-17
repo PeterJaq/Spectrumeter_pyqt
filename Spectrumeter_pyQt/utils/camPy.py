75e1f927f39565387d2ba4425760212cacdafa33
peterjaq <anqingjiang0524@akane.waseda.jp>
2018-09-17 23:50:51 +0800

    9.17 add D90 API: set_image_format / get_image

--- Spectrumeter_pyQt/utils/camPy.py
+++ Spectrumeter_pyQt/utils/camPy.py
@@ -1,27 +1,67 @@
 import ctypes
+import string
+import time
 
 class Spect_cam_api:
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, mExposure = ctypes.c_int(10)):
         lib_loader = ctypes.cdll.LoadLibrary
-        self.cam_api = lib_loader("../libs/400A/include/CamApi.h")
+        self.cam_api = lib_loader("../libs/400A/lib/Camkernel.dll")
+        self.mExposure = mExposure
+        self.dev_name = None
         self.init_cam()
 
-        
-        
-    def init_cam():
+    def init_cam(self):
 
         self.cam_api.InitDevice("")
         total = self.cam_api.DeviceCount()
-        if(total is True):
+
+        if total is 1:
             print("Spectrumeter connect is Success!" )
-            return True
         else:
             print("WARNING!: Spectrumeter connect is not success!")
-            return False
 
-    def get_cam_info():
-        
-        self.DeviceProduct = self.cam_api.DeviceProduct
+        self.cam_api.ExitDevice()
+        self.cam_api.InitDevice()
+        self.dev_name = self.cam_api.DeviceName(0)
+        self.cam_api.ResetDevice(self.dev_name)
+        self.cam_api.SetExposure(self.mExposure, self.dev_name)
+        self.cam_api.SetCooled(ctypes.c_bool(self.get_cam_temp().value >= -20), self.dev_name)
 
         return True
+
+    def close_cam(self):
+        self.cam_api.SuspendDevice(self.dev_name)
+
+        return True
+
+    def get_cam_temp(self):
+        temperature = ctypes.c_float()
+
+        self.cam_api.GetTemperature(ctypes.byref(temperature), self.dev_name)
+
+        return temperature
+
+    def set_image_format(self, imgType=ctypes.c_int(0)):
+        """
+        :param imgType: [SNAP_RAW, SNAP_BMP, SNAP_JPG, SNAP_TIF, SNAP_PNG]
+
+                        image formats include RAW, BMP, JPG, TIF, PNG
+
+        """
+
+        return self.cam_api.SetImageFormat(imgType, self.dev_name)
+
+    def save_image(self, path="../image/"):
+        """
+        API: void SetImageFormat(int picType, const char *devName)
+        :param path: image path
+        :return:
+        """
+
+        return self.cam_api.SaveImage(path, 0, self.dev_name)
+
+
+
+
+
